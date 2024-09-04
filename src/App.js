6fa256b58ab170b7bdfdd42bcbbba18fa20eee7d
Hassaan <87420500+hassaan871@users.noreply.github.com>
2024-09-04 13:50:12 +0500

    basic Routing added in App.js

--- src/App.js
+++ src/App.js
@@ -5,6 +5,12 @@ import Login from "./components/authentication/login/Login";
 import SignUp from "./components/authentication/signup/SignUp";
 import UserProfileCompletion from "./components/authentication/signup/UserProfileCompletion";
 import DoctorDashboard from "./components/doctor/DoctorDashboard/DoctorDashboard";
+import DoctorChat from "./components/doctor/DoctorChat/DoctorChat";
+import DoctorChatWithPatientDetails from "./components/doctor/DoctorChatWithPatientDetails/DoctorChatWithPatientDetails";
+import DoctorDetailEdit from "./components/doctor/DoctorDetailEdit/DoctorDetailEdit";
+import DoctorDetailView from "./components/doctor/DoctorDetailView/DoctorDetailView";
+import DoctorPanel from "./components/doctor/DoctorPanel/DoctorPanel";
+import PatientDetails from "./components/doctor/PatientDetails/PatientDetails";
 
 function App() {
   return (
@@ -15,6 +21,12 @@ function App() {
         <Route path="/signup" element={<SignUp />} />
         <Route path="/profile-completion" element={<UserProfileCompletion />} />
         <Route path="/dashboard" element={<DoctorDashboard />} />
+        <Route path="/doctorchat" element={<DoctorChat/>} />
+        <Route path="/doctorchatwithpatientdetail" element={<DoctorChatWithPatientDetails/>} />
+        <Route path="/doctordetailedit" element={<DoctorDetailEdit/>} />
+        <Route path="/doctordetailview" element={<DoctorDetailView/>} />
+        <Route path="/doctorpanel" element={<DoctorPanel/>} />
+        <Route path="/patientdetails" element={<PatientDetails/>} />
       </Routes>
     </Router>
   );
