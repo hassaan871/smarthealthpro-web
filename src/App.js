cd2088998074753ec037daa68c5f3c963efff0fd
Hassaan <87420500+hassaan871@users.noreply.github.com>
2024-10-06 15:47:22 +0500

    Refactor: Replaced custom styles with Bootstrap, removed redundant files, and optimized components

--- src/App.js
+++ src/App.js
@@ -1,32 +1,24 @@
 // src/App.js
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import Login from "./components/authentication/login/Login";
 import SignUp from "./components/authentication/signup/SignUp";
 import UserProfileCompletion from "./components/authentication/signup/UserProfileCompletion";
-import DoctorChat from "./components/doctor/DoctorChat/DoctorChat";
 import DoctorChatWithPatientDetails from "./components/doctor/DoctorChatWithPatientDetails/DoctorChatWithPatientDetails";
-import DoctorProfile from './components/doctor/DoctorProfile/DoctorProfile';
-import Nav from './components/Navbar/Nav';
 import Dashboard from './components/dashboard/Dashboard';
 
 function App() {
   return (
     <Router>
       <Routes>
+        <Route path="/" element={<Navigate to='/login' />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<SignUp />} />
         <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/profile-completion" element={<UserProfileCompletion />} />
-        <Route path="/doctorchat" element={<DoctorChat/>} />
         <Route path="/doctorchatwithpatientdetail" element={<DoctorChatWithPatientDetails/>} />
-        <Route path="/doctorprofile" element={<DoctorProfile/>} />
+        <Route path="/profile-completion" element={<UserProfileCompletion />} />
       </Routes>
     </Router>
-    // <>
-    // <Nav/>
-    // <DoctorProfile/>
-    // </>
   );
 }
 
