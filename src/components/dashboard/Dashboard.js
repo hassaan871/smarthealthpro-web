b3ca4023434839e98dc5e1f95757719a8522ac95
Hassaan <87420500+hassaan871@users.noreply.github.com>
2024-10-06 14:31:42 +0500

    Refactor Appointments Progress page: replaced custom CSS component with Bootstrap used component AppointmentProgressWidget and deleted Appointments.css and Appointments.js files

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -14,7 +14,6 @@ const Dashboard = () => {
             case 'Overview':
                 return <Overview />
             case 'Appointments':
-                // return <Appointments />;
                 return <AppointmentProgressWidget />;
                 // return <AppointmentProgressWidget style={{ width: '100%' }} />;
             case 'Patients':
